329eb73acb2580135417667b47ef318b2111b862
futinuti <amihailovp@gmail.com>
2023-01-01 20:17:23 +0200

    исправил домашка 5

--- dz_5.py
+++ dz_5.py
@@ -1,14 +1,15 @@
 def max_letters(stroka):
     stroka = stroka.lower()
     stroka = stroka.split(" ")
-    str_schet = []
+
+    di = {}
     for i in stroka:
         schet = 0
         for j in stroka:
             if i == j:
                 schet += 1
-        str_schet.append(schet)
-    di = dict(zip(stroka, str_schet))
+        di[i] = schet
+
     maks = 0
     max_i = []
     for i in di.items():
