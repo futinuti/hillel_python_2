3f5ae6942be72f4f08512123a29b9ec305b71e95
futinuti <amihailovp@gmail.com>
2023-01-01 19:18:44 +0200

    добавил домашка 4

---
+++ dz_4.py
@@ -0,0 +1,23 @@
+class DigitalCounter:
+    def __init__(self, start=0, end=100, current=None):
+        self.start = start
+        self.end = end
+        self.current = current
+        if self.current is None:
+            self.current = self.start
+
+    def increase(self):
+        self.current += 1
+        if self.current >= self.end:
+            self.current = self.start
+
+    def get_current_value(self):
+        print(f'Текущее значение {self.current}')
+
+
+if __name__ == '__main__':
+    c = DigitalCounter()
+    c.get_current_value()
+    for i in range(111):
+        c.increase()
+    c.get_current_value()
